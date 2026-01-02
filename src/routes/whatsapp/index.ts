aeddcd38b9758ad5b872e2a2d9bb10ca44a2d072
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2026-01-03 01:18:20 +0530

    url fix

--- src/routes/whatsapp/index.ts
+++ src/routes/whatsapp/index.ts
@@ -101,12 +101,12 @@ router.get('/', async (req: Request, res: Response) => {
   res.status(200).send('WhatsApp webhook forwarding service');
 });
 
-const messagesUrl = 'https://dev.brijesh.fun/webhook/whatsapp';
+const messagesUrl : string = 'https://subs-local-backend.brijesh.fun/webhook/whatsapp';
 router.post('/', async (req: Request, res: Response) => {
   await handleWebhook(req, res, process.env.TARGET_URL_WHATSAPP_MESSAGES || messagesUrl);
 });
 
-const convoUrl = 'https://dev.brijesh.fun/webhook/whatsapp/conv';
+const convoUrl : string = 'https://subs-local-backend.brijesh.fun/webhook/whatsapp';
 router.post('/conv', async (req: Request, res: Response) => {
   await handleWebhook(req, res, process.env.TARGET_URL_WHATSAPP_CONVO || convoUrl);
 });
